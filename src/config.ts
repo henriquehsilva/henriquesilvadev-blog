7b520c6604161c2856118be3e0cd7bd5c0bb6048
Henrique Silva <henrique@henriquesilva.dev>
2023-07-18 12:05:14 -0300

    Update links

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Henrique Silva Dev | Personal Website';
-export const SITE_DESCRIPTION = 'Henrique Silva Dev is a Personal Website built with Astro and TailwindCSS.';
+export const SITE_DESCRIPTION = 'Henrique Silva Dev is a Personal.';
