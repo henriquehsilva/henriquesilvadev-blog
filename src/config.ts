8c3cf5d125cf77232af4d4d1c1bb2c2361610442
Henrique Silva <henrique@henriquesilva.dev>
2023-07-19 21:13:50 -0300

    Update description head

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'HenriqueSilva.dev';
-export const SITE_DESCRIPTION = 'Henrique Silva Dev is a Personal.';
+export const SITE_DESCRIPTION = 'Henrique Silva, um programador escrevendo algo além de código.';
