83c26cea7486d06752d676aff6961d4ffb34e7d5
Henrique Silva <henrique@henriquesilva.dev>
2023-11-26 21:59:50 -0300

    Edita astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import rehypePrettyCode from "rehype-pretty-code";
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -16,5 +17,8 @@ export default defineConfig({
       cacheDir: "./.cache/image",
       logLevel: 'debug',
     }
-  )]
+  )], 
+  mdx: {
+    rehypePlugins: [rehypePrettyCode],
+  },
 });
