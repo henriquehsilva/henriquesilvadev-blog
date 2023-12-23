f092f1c55c69a3ed6b3a2b2661a436435df4a432
Henrique Honorio da Silva <henriquesilvadev@macbookair.local>
2023-12-23 15:09:12 -0300

    Editando description

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'HenriqueSilva.dev';
-export const SITE_DESCRIPTION = 'Henrique Silva, um programador tentando escrever algo além de código, e desenvolver algo além de software.';
+export const SITE_TITLE = 'HENRIQUE SILVA DEV';
+export const SITE_DESCRIPTION = 'Henrique Silva, um programador tentando escrever algo além de código, e criar algo além de software.';
