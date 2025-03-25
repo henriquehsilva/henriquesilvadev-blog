721971d160e68145565928996ef28a01cedeb3ed
Henrique Honorio da Silva <contato@henriquesilva.dev>
2025-03-25 11:21:59 -0300

    add card

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'HENRIQUE SILVA DEV';
-export const SITE_DESCRIPTION = 'Henrique Silva, um programador tentando escrever algo além de código, e criar algo além de software.';
+export const SITE_DESCRIPTION = 'Conheça meu trabalho com interfaces inteligentes, chatbots e experiências conversacionais.';
+export const SITE_URL = 'https://henriquesilva.dev';
+export const SITE_IMAGE = '/me.png';
